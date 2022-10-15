8d158b128616b04b15c4bb1e4ed8d8a883bec1f6
joweich <jns.wch@gmail.com>
2022-10-16 01:16:33 +0200

    applied black formatter

--- chatminer/chatparsers.py
+++ chatminer/chatparsers.py
@@ -7,6 +7,7 @@ from enum import Enum
 from pathlib import Path
 from dateutil import parser as datetimeparser
 import pandas as pd
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -18,8 +19,11 @@ class Parser(ABC):
         self.messages = None
         self.df = None
 
-        self._logger = logging.getLogger(__name__).\
-            getChild(self.__class__.__name__).getChild(str(id(self)))
+        self._logger = (
+            logging.getLogger(__name__)
+            .getChild(self.__class__.__name__)
+            .getChild(str(id(self)))
+        )
 
     def parse_file_into_df(self):
         self._read_file_into_list()
@@ -42,11 +46,10 @@ class Parser(ABC):
             self._logger.error("Failed writing to csv. Parse file first.")
 
     def _add_metadata(self):
-        self.df['weekday'] = self.df['datetime'].dt.day_name()
+        self.df["weekday"] = self.df["datetime"].dt.day_name()
         self.df["hour"] = self.df["datetime"].dt.hour
-        self.df['words'] = \
-            self.df['message'].apply(lambda s: len(s.split(' ')))
-        self.df['letters'] = self.df['message'].apply(len)
+        self.df["words"] = self.df["message"].apply(lambda s: len(s.split(" ")))
+        self.df["letters"] = self.df["message"].apply(len)
 
     @abstractmethod
     def _read_file_into_list(self):
@@ -56,8 +59,9 @@ class Parser(ABC):
     def _parse_message(self, mess):
         return
 
+
 class SignalParser(Parser):
-    DATEREG = r'^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\]'
+    DATEREG = r"^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\]"
 
     def _read_file_into_list(self):
         self.messages = []
@@ -72,7 +76,7 @@ class SignalParser(Parser):
                 if buffer:
                     buffer.append(line)
                     buffer.reverse()
-                    self.messages.append(' '.join(buffer))
+                    self.messages.append(" ".join(buffer))
                     buffer.clear()
                 else:
                     self.messages.append(line)
@@ -81,21 +85,17 @@ class SignalParser(Parser):
         self._logger.info(f"Finished reading {len(self.messages)} messages.")
 
     def _parse_message(self, mess):
-        datetime_raw, mess = mess.split(']', 1)
+        datetime_raw, mess = mess.split("]", 1)
         time = datetimeparser.parse(datetime_raw[1:])
-        author, mess = mess.split(':', 1)
+        author, mess = mess.split(":", 1)
         author = author.strip()
         mess = mess.strip()
-        parsed_message = {
-            'datetime': time,
-            'author': author,
-            'message': mess
-        }
+        parsed_message = {"datetime": time, "author": author, "message": mess}
         return parsed_message
 
 
 class WhatsAppParser(Parser):
-    DATEREG = r'^((\d{1})|(\d{2}))((\.)|(\/))((\d{1})|(\d{2}))((\.)|(\/))((\d{2}))'
+    DATEREG = r"^((\d{1})|(\d{2}))((\.)|(\/))((\d{1})|(\d{2}))((\.)|(\/))((\d{2}))"
 
     def __init__(self, filepath):
         super().__init__(filepath)
@@ -133,7 +133,7 @@ class WhatsAppParser(Parser):
                 if buffer:
                     buffer.append(line)
                     buffer.reverse()
-                    self.messages.append(' '.join(buffer))
+                    self.messages.append(" ".join(buffer))
                     buffer.clear()
                 else:
                     self.messages.append(line)
@@ -146,60 +146,59 @@ class WhatsAppParser(Parser):
         max_first = 0
         max_second = 0
         for line in self.messages:
-            line = line.replace(r'/', '.', 2)
-            day_and_month = [int(num) for num in line.split('.')[:2]]
+            line = line.replace(r"/", ".", 2)
+            day_and_month = [int(num) for num in line.split(".")[:2]]
             max_first = max(max_first, day_and_month[0])
             max_second = max(max_second, day_and_month[1])
             if (max_first > 12) and (max_second > 12):
                 raise ValueError(f"Invalid date format: {line}")
 
-        if (max_first > 12 and max_second <= 12):
+        if max_first > 12 and max_second <= 12:
             self._logger.info("Inferred day first format.")
             self._datetime_format = StartOfDateType.DAY
-        elif (max_first <= 12 and max_second > 12):
+        elif max_first <= 12 and max_second > 12:
             self._logger.info("Inferred month first format.")
             self._datetime_format = StartOfDateType.MONTH
         else:
-            self._logger.warning("Can't infer dateformat. \
-                No message with day > 12. Assuming day first.")
+            self._logger.warning(
+                "Can't infer dateformat. \
+                No message with day > 12. Assuming day first."
+            )
             self._datetime_format = StartOfDateType.AMBIGUOUS
 
     def _parse_message(self, mess):
-        if self._datetime_format in (StartOfDateType.DAY,
-                                     StartOfDateType.AMBIGUOUS):
-            time = datetimeparser.parse(mess.split('-', 1)[0],
-                                            dayfirst=True, fuzzy=True)
+        if self._datetime_format in (StartOfDateType.DAY, StartOfDateType.AMBIGUOUS):
+            time = datetimeparser.parse(
+                mess.split("-", 1)[0], dayfirst=True, fuzzy=True
+            )
         else:
-            time = datetimeparser.parse(mess.split('-', 1)[0],
-                                            dayfirst=False, fuzzy=True)
+            time = datetimeparser.parse(
+                mess.split("-", 1)[0], dayfirst=False, fuzzy=True
+            )
 
         author = self._get_message_author(mess)
-        if author != 'System':
-            body = mess.split('-', 1)[1].split(':', 1)[1].strip()
+        if author != "System":
+            body = mess.split("-", 1)[1].split(":", 1)[1].strip()
         else:
-            if len(mess.split('-', 1)) == 1:
+            if len(mess.split("-", 1)) == 1:
                 self._logger.warning(f"Failed to parse message: {mess}.")
                 self._logger.warning("Please report message format in GitHub.")
                 return None
-            body = mess.split('-', 1)[1]
+            body = mess.split("-", 1)[1]
 
-        parsed_message = {
-            'datetime': time,
-            'author': author,
-            'message': body
-        }
+        parsed_message = {"datetime": time, "author": author, "message": body}
         return parsed_message
 
     def _get_message_author(self, message):
         patterns = [
-            r'- ([+])([0-9 ]+)(:)',
-            r'- ([\w]+):',
-            r'- ([\w]+[\s]+[\w]+):',
-            r'- ([\w]+[\s]+[\w]+[\s]+[\w]+):',
+            r"- ([+])([0-9 ]+)(:)",
+            r"- ([\w]+):",
+            r"- ([\w]+[\s]+[\w]+):",
+            r"- ([\w]+[\s]+[\w]+[\s]+[\w]+):",
         ]
-        pattern = '|'.join(patterns)
+        pattern = "|".join(patterns)
         res = re.search(pattern, message)
-        return re.sub(r'|\-|\:', '', res.group(0)).strip() if res else 'System'
+        return re.sub(r"|\-|\:", "", res.group(0)).strip() if res else "System"
 
 
 class FacebookMessengerParser(Parser):
@@ -214,33 +213,33 @@ class FacebookMessengerParser(Parser):
         self._logger.info(f"Finished reading {len(self.messages)} messages.")
 
     def _parse_message(self, mess):
-        if mess['type'] == 'Share':
-            body = mess['share']['link']
-        elif 'sticker' in mess:
-            body = mess['sticker']['uri']
+        if mess["type"] == "Share":
+            body = mess["share"]["link"]
+        elif "sticker" in mess:
+            body = mess["sticker"]["uri"]
         else:
-            body = mess['content']
+            body = mess["content"]
 
         parsed_message = {
-            'datetime': datetime.datetime.fromtimestamp(mess['timestamp_ms']/1000),
-            'author': mess["sender_name"],
-            'message': body
+            "datetime": datetime.datetime.fromtimestamp(mess["timestamp_ms"] / 1000),
+            "author": mess["sender_name"],
+            "message": body,
         }
         return parsed_message
 
 
 class TelegramJsonParser(Parser):
     def _read_file_into_list(self):
-        with self._file.open(encoding='utf-8') as f:
+        with self._file.open(encoding="utf-8") as f:
             json_objects = json.load(f)
-            self.messages = json_objects['messages']
+            self.messages = json_objects["messages"]
             self._logger.info(f"Finished reading {len(self.messages)} messages.")
 
     def _parse_message(self, mess):
         parsed_message = {
-            'datetime': mess['from'],
-            'author': datetime.datetime.fromtimestamp(int(mess['date_unixtime'])),
-            'message': mess['text']
+            "datetime": mess["from"],
+            "author": datetime.datetime.fromtimestamp(int(mess["date_unixtime"])),
+            "message": mess["text"],
         }
         return parsed_message
 
