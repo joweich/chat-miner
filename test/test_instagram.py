a812fc4f70cf40e6f3d0b65f9a044ae4f2a4aab3
Massimo Pavoni <31775087+massimopavoni@users.noreply.github.com>
2023-03-15 20:20:25 +0100

    Moved parsers' test methods into dedicated files (#91)

---
+++ test/test_instagram.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from chatminer.chatparsers import InstagramJsonParser
+
+
+def test_instagram():
+    parser = InstagramJsonParser("test/instagram/testlog.json")
+    parser.parse_file()
+    df_res = parser.parsed_messages.get_df()
+    df_test = pd.read_csv(
+        "test/instagram/testlog_target.csv",
+        parse_dates=["timestamp"],
+        infer_datetime_format=True,
+    )
+    assert_frame_equal(
+        df_test[["author", "message", "words", "letters"]],
+        df_res[["author", "message", "words", "letters"]],
+    )
---
+++ test/test_whatsapp.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from chatminer.chatparsers import WhatsAppParser
+
+
+def assert_equal_from_file(file):
+    parser = WhatsAppParser(f"test/whatsapp/test_{file}.txt")
+    parser.parse_file()
+    df_res = parser.parsed_messages.get_df()
+    df_test = pd.read_csv(
+        f"test/whatsapp/test_{file}_target.csv",
+        parse_dates=["timestamp"],
+        infer_datetime_format=True,
+    )
+    assert_frame_equal(df_test, df_res)
+
+
+def test_dateformat1():
+    assert_equal_from_file("dateformat1")
+
+
+def test_dateformat2():
+    assert_equal_from_file("dateformat2")
+
+
+def test_dateformat3():
+    assert_equal_from_file("dateformat3")
+
+
+def test_dateformat4():
+    assert_equal_from_file("dateformat4")
+
+
+def test_dateformat5():
+    assert_equal_from_file("dateformat5")
+
+
+def test_unicode():
+    assert_equal_from_file("unicode")
