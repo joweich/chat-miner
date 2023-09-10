b0d629efac05d9050bc224733eb393f79f81b501
Jonas Weich <jns.wch@gmail.com>
2023-09-10 22:41:34 +0200

    Fixed formatting

--- chatminer/cli.py
+++ chatminer/cli.py
@@ -1,69 +1,80 @@
 #!/usr/bin/env python3
 
-import argparse,sys
-from chatminer.chatparsers import WhatsAppParser,FacebookMessengerParser,InstagramJsonParser,SignalParser,TelegramJsonParser
+import argparse, sys
+from chatminer.chatparsers import (
+    WhatsAppParser,
+    FacebookMessengerParser,
+    InstagramJsonParser,
+    SignalParser,
+    TelegramJsonParser,
+)
+
 
 def get_args():
-        
     try:
-        #Initialising the argument parser
-        parser=argparse.ArgumentParser(description="chat-miner provides lean parsers for every major platform transforming chats into pandas dataframes. Artistic visualizations allow you to explore your data and create artwork from your chats.")
+        parser = argparse.ArgumentParser(
+            description="chat-miner provides lean parsers for every major platform transforming chats into pandas dataframes.\
+             Artistic visualizations allow you to explore your data and create artwork from your chats."
+        )
 
-        #Adding the argument for the name of the parser 
-        parser.add_argument('-p','--parser',type=str,help='The platform from which the chats are imported',choices=['whatsapp','instagram','facebook','signal','telegram'])
+        parser.add_argument(
+            "-p",
+            "--parser",
+            type=str,
+            help="The platform from which the chats are imported",
+            choices=["whatsapp", "instagram", "facebook", "signal", "telegram"],
+        )
 
-        #Adding the argument for the name of the input file
-        parser.add_argument('-i','--input',type=str,help='Input file to be processed')
+        parser.add_argument(
+            "-i", "--input", type=str, help="Input file to be processed"
+        )
 
-        #adding the argument for the name of the output file
-        parser.add_argument('-o','--output',type=str,help='Output file for the results')
+        parser.add_argument(
+            "-o", "--output", type=str, help="Output file for the results"
+        )
 
-        return parser.parse_args(),parser
+        return parser.parse_args(), parser
     except:
         sys.exit()
 
 
 def main():
-
     try:
-        args,parser=get_args()
+        args, parser = get_args()
 
-        parser_type=args.parser
-        INPUT_FILE=args.input
-        OUTPUT_FILE=args.output
+        parser_type = args.parser
+        INPUT_FILE = args.input
+        OUTPUT_FILE = args.output
 
-        if INPUT_FILE==None or OUTPUT_FILE==None or parser_type==None:
+        if INPUT_FILE == None or OUTPUT_FILE == None or parser_type == None:
             raise ValueError
 
-        #Logic to parse the file and make a csv file
-        if((parser_type).lower()=='whatsapp'):
-            parser_type=WhatsAppParser(INPUT_FILE)
+        if (parser_type).lower() == "whatsapp":
+            parser_type = WhatsAppParser(INPUT_FILE)
             parser_type.parse_file()
-            output=parser_type.parsed_messages.get_df()
+            output = parser_type.parsed_messages.get_df()
 
-        elif((parser_type).lower()=='facebook'):
-            parser_type=FacebookMessengerParser(INPUT_FILE)
+        elif (parser_type).lower() == "facebook":
+            parser_type = FacebookMessengerParser(INPUT_FILE)
             parser_type.parse_file()
-            output=parser_type.parsed_messages.get_df()
+            output = parser_type.parsed_messages.get_df()
 
-        elif((parser_type).lower()=='instagram'):
-            parser_type=InstagramJsonParser(INPUT_FILE)
+        elif (parser_type).lower() == "instagram":
+            parser_type = InstagramJsonParser(INPUT_FILE)
             parser_type.parse_file()
-            output=parser_type.parsed_messages.get_df()
+            output = parser_type.parsed_messages.get_df()
 
-        elif((parser_type).lower()=='signal'):
-            parser_type=SignalParser(INPUT_FILE)
+        elif (parser_type).lower() == "signal":
+            parser_type = SignalParser(INPUT_FILE)
             parser_type.parse_file()
-            output=parser_type.parsed_messages.get_df()
+            output = parser_type.parsed_messages.get_df()
 
-        elif((parser_type).lower()=='telegram'):
-            parser_type=TelegramJsonParser(INPUT_FILE)
+        elif (parser_type).lower() == "telegram":
+            parser_type = TelegramJsonParser(INPUT_FILE)
             parser_type.parse_file()
-            output=parser_type.parsed_messages.get_df()
+            output = parser_type.parsed_messages.get_df()
 
-        #csv file made and saved to the path specified by the user
-        output.to_csv(OUTPUT_FILE,index=False)
-            
+        output.to_csv(OUTPUT_FILE, index=False)
 
     except ValueError:
         parser.print_usage()
@@ -71,5 +82,6 @@ def main():
     except:
         sys.exit()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="chat-miner",
-    description="chat-miner provides lean parsers for every major platform transforming chats into pandas dataframes. Artistic visualizations allow you to explore your data and create artwork from your chats.",
+    description="chat-miner provides lean parsers for every major platform transforming chats into pandas dataframes.\
+         Artistic visualizations allow you to explore your data and create artwork from your chats.",
     packages=find_packages(),
     install_requires=[
-        "argparse",  # argparse is part of the Python standard library, so it's not in requirements.txt
+        "argparse",
     ],
     entry_points={
         "console_scripts": [
