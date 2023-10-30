b47b74fd7e2d08c3dae7cdc1d98c793cde3f3376
Jonas Weich <Jonas.weich@outlook.de>
2023-10-30 11:34:58 +0200

    Extended smoketest to check viz function availability

--- test/vis/test_smoketest.py
+++ test/vis/test_smoketest.py
@@ -4,7 +4,10 @@ import pytest
 
 def test_visualization_import():
     try:
-        from chatminer import visualizations
-        assert "visualizations" in sys.modules
+        import chatminer.visualizations as vis
+        assert hasattr(vis, "sunburst")
+        assert hasattr(vis, "wordcloud")
+        assert hasattr(vis, "calendar_heatmap")
+        assert hasattr(vis, "radar")
     except ImportError as e:
         pytest.fail(f"Error importing visualizations: {e}")
