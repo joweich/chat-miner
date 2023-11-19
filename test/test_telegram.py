9b6cc32f51ecf1d68376539aae3c62da522afe47
Jonas Weich <jns.wch@gmail.com>
2023-11-19 22:17:19 +0100

    Add Telegram tests

---
+++ test/test_telegram.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from chatminer.chatparsers import TelegramJsonParser
+
+
+def test_telegram_single_export():
+    parser = TelegramJsonParser("test/telegram/test_single_export.json")
+    parser.parse_file()
+    df_res = parser.parsed_messages.get_df()
+    df_test = pd.read_csv(
+        "test/telegram/test_target.csv",
+        parse_dates=["timestamp"],
+    )
+    assert_frame_equal(
+        df_test[["author", "message", "words", "letters"]],
+        df_res[["author", "message", "words", "letters"]],
+    )
+
+
+def test_telegram_batch_export():
+    parser = TelegramJsonParser("test/telegram/test_single_export.json", "Chatname")
+    parser.parse_file()
+    df_res = parser.parsed_messages.get_df()
+    df_test = pd.read_csv(
+        "test/telegram/test_target.csv",
+        parse_dates=["timestamp"],
+    )
+    assert_frame_equal(
+        df_test[["author", "message", "words", "letters"]],
+        df_res[["author", "message", "words", "letters"]],
+    )
