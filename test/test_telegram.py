030c7b3ea324a1556f7b9e3fd8cf7b2c2cda16fe
Jonas Weich <jns.wch@gmail.com>
2023-12-03 22:36:37 +0100

    Refactor Telegram tests

--- test/test_telegram.py
+++ test/test_telegram.py
@@ -1,32 +1,17 @@
-import pandas as pd
-from pandas.testing import assert_frame_equal
-
-from chatminer.chatparsers import TelegramJsonParser
+from chatminer.chatparsers import ParsedMessageCollection, TelegramJsonParser
 
 
 def test_telegram_single_export():
+    target = ParsedMessageCollection()
+    target.read_from_json("test/telegram/target.json")
     parser = TelegramJsonParser("test/telegram/test_single_export.json")
     parser.parse_file()
-    df_res = parser.parsed_messages.get_df()
-    df_test = pd.read_csv(
-        "test/telegram/test_target.csv",
-        parse_dates=["timestamp"],
-    )
-    assert_frame_equal(
-        df_test[["author", "message", "words", "letters"]],
-        df_res[["author", "message", "words", "letters"]],
-    )
+    assert parser.parsed_messages == target
 
 
 def test_telegram_batch_export():
+    target = ParsedMessageCollection()
+    target.read_from_json("test/telegram/target.json")
     parser = TelegramJsonParser("test/telegram/test_batch_export.json", "Chatname")
     parser.parse_file()
-    df_res = parser.parsed_messages.get_df()
-    df_test = pd.read_csv(
-        "test/telegram/test_target.csv",
-        parse_dates=["timestamp"],
-    )
-    assert_frame_equal(
-        df_test[["author", "message", "words", "letters"]],
-        df_res[["author", "message", "words", "letters"]],
-    )
+    assert parser.parsed_messages == target
